b2fdbc4a9137c09699967c4e4364ba9d60c89597
joubtasn996 <100589091+joubtasn996@users.noreply.github.com>
2022-04-01 08:43:28 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,7 +1,9 @@
 def main():
     #your code goes here
     number = int(input("Enter a Number:"))
-    if (number % 2) == 0:
+    if number % 3 == 0 and number % 5 == 0:
+      print("fizzbuzz")
+    elif (number % 2) == 0:
       print("2")
     elif (number % 3) == 0:
       print("fizz")
@@ -9,8 +11,6 @@ def main():
       print("4")
     elif (number % 5) == 0:
       print("buzz")
-    elif (number % 3 and number % 5) == 0:
-      print("fizzbuzz")
     else:
       print(number)
 if __name__ =='__main__':
