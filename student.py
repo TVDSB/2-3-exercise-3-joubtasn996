089e92754b6bbf20e7135b5346ebac0283177b81
joubtasn996 <100589091+joubtasn996@users.noreply.github.com>
2022-04-01 08:37:19 -0400

    Update student.py

--- student.py
+++ student.py
@@ -4,15 +4,14 @@ def main():
     if (number % 2) == 0:
       print("2")
     elif (number % 3) == 0:
-      print("Fizz")
+      print("fizz")
     elif (number % 4) == 0:
       print("4")
     elif (number % 5) == 0:
-      print("Buzz")
-    elif (number % 5 and number % 3) == 0:
-      print("BuzzFizz")
+      print("buzz")
+    elif (number % 3 and number % 5) == 0:
+      print("fizzbuzz")
     else:
       print(number)
-
 if __name__ =='__main__':
     main()
