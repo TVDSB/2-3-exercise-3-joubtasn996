44240beaa76441e818d60a4ea99ba8a620b9def7
joubtasn996 <100589091+joubtasn996@users.noreply.github.com>
2022-04-01 08:47:34 -0400

    Update student.py

--- student.py
+++ student.py
@@ -3,12 +3,8 @@ def main():
     number = int(input("Enter a Number:"))
     if number % 3 == 0 and number % 5 == 0:
       print("fizzbuzz")
-    elif (number % 2) == 0:
-      print("2")
     elif (number % 3) == 0:
       print("fizz")
-    elif (number % 4) == 0:
-      print("4")
     elif (number % 5) == 0:
       print("buzz")
     else:
